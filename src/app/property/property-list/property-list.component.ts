d28d1883ddc11b514cbb05d74de834c2b57d728b
vs user <vs-user@aws.com>
2020-07-21 17:46:59 +0100

    Save new property and display on list page and Saving multiple properties

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { HousingService } from 'src/app/services/housing.service';
-import { IProperty } from '../IProperty.interface';
 import { ActivatedRoute } from '@angular/router';
-//import { HttpClient } from '@angular/common/http';
+import { IPropertyBase } from 'src/app/model/ipropertybase';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css'],
+  styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  SellRent =1;
-  properties: Array<IProperty>;
+  SellRent = 1;
+  properties: IPropertyBase[];
 
-  constructor(private route: ActivatedRoute ,private housingService: HousingService) {  }
+  constructor(private route: ActivatedRoute, private housingService: HousingService) { }
 
   ngOnInit(): void {
-    if(this.route.snapshot.url.toString())
-    {
-      this.SellRent = 2; //2 refers to rent-property url otherwise Base url ie: Buy
+    if (this.route.snapshot.url.toString()) {
+      this.SellRent = 2; // Means we are on rent-property URL else we are on base URL
     }
     this.housingService.getAllProperties(this.SellRent).subscribe(
-      data=>{
-           this.properties= data;
-           console.log(data);
-      },error =>{
+        data => {
+        this.properties = data;
+        const newProperty = JSON.parse(localStorage.getItem('newProp'));
+
+        if (newProperty.SellRent === this.SellRent) {
+          this.properties = [newProperty, ...this.properties];
+        }
+
+        console.log(data);
+      }, error => {
         console.log('httperror:');
         console.log(error);
       }
--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,26 +1,36 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { IProperty } from '../property/IProperty.interface';
-import { Observable } from 'rxjs/internal/Observable';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { Observable } from 'rxjs';
+import { IProperty } from '../model/iproperty';
+import { IPropertyBase } from '../model/ipropertybase';
+import { Property } from '../model/property';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class HousingService {
-  constructor(private http: HttpClient) {}
 
-  getAllProperties(SellRent: number): Observable<IProperty[]> {
+  constructor(private http: HttpClient) { }
+
+  getAllProperties(SellRent: number): Observable<IPropertyBase[]> {
     return this.http.get('data/properties.json').pipe(
-      map((data) => {
-        const propertiesArray: Array<IProperty> = [];
-        for (const id in data) {
-          if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
-            propertiesArray.push(data[id]);
-          }
+      map(data => {
+      const propertiesArray: Array<IPropertyBase> = [];
+
+      for (const id in data) {
+        if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
+          propertiesArray.push(data[id]);
         }
-        return propertiesArray;
+      }
+      return propertiesArray;
       })
     );
+
+    return this.http.get<IProperty[]>('data/properties.json');
+  }
+  addProperty(property: Property) {
+    localStorage.setItem('newProp', JSON.stringify(property));
   }
 }
