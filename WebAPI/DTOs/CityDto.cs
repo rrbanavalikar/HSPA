345f47f5fdb02d2f4bbb6172f2b399ba89912db9
vs user <rr.banavalikar@gmail.com>
2020-12-13 18:17:38 +0000

    Input validation and exception handling in ASP.NET Web API

--- WebAPI/DTOs/CityDto.cs
+++ WebAPI/DTOs/CityDto.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebAPI.DTOs
 {
     public class CityDto
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Name is a mandatory field")]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression(".*[a-zA-Z]+.*", ErrorMessage = "Numbers are not allowed in Name")]
         public string Name { get; set; }
 
+        [Required]
         public string Country  { get; set; }
     }
 }
