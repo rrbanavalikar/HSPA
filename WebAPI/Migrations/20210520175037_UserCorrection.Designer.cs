dad1fe2e044c6e7ca3b593ada4958266f9420307
vs user <rr.banavalikar@gmail.com>
2021-05-21 16:47:25 +0100

    Database seeding in Entity Framework Core | Angular + WebAPI Tutorial

--- WebAPI/Migrations/20210520175037_UserCorrection.Designer.cs
+++ WebAPI/Migrations/20210520175037_UserCorrection.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPI.Data;
 
 namespace WebAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210520175037_UserCorrection")]
+    partial class UserCorrection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
